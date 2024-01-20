3508ad56f7559018ccab54ed9c104550d6b0de56
Metz I <128658219+metz-i@users.noreply.github.com>
2024-01-19 20:55:47 -0500

    added props to parent and child components

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,14 @@
-function ProfileCard() {
-    return <div>Profile Card</div>
-}
+function ProfileCard({ title, handle }) {
+    //const title = props.title;
+    //const handle = props.handle;
+    // const { title, handle } = props;
+    console.log(title, handle)
+    return (
+        <div>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
+        </div>
+    )
+    };
 
 export default ProfileCard;
